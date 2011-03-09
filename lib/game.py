4a62cb0224ca522ff75007c8386e797eda9c231d
Danilo Cabello <danilo.cabello@gmail.com>
2011-03-09 00:42:06 -0300

    Add drop_piece tests and logic.

--- lib/game.py
+++ lib/game.py
@@ -1,6 +1,26 @@
 # -*- coding: utf-8 -*-
 
+class ColumnFullException(Exception):
+    pass
+
 class Game:
     def __init__(self, rows=6, columns=7):
-        self._rows = 6
-        self._columns = 7
+        self._rows = rows
+        self._columns = columns
+        self._pieces = []
+
+    def _drop_piece(self, piece, column):
+        position = None
+
+        # from rows to zero
+        for y in range(self._rows-1, -1, -1):
+            if (column, y) not in self._pieces:
+                position = (column, y)
+                self._pieces.append(position)
+                break
+
+        if position is None:
+            raise ColumnFullException()
+
+        return position
+
--- tests/game_test.py
+++ tests/game_test.py
@@ -2,7 +2,7 @@
 
 import unittest
 
-from lib.game import Game
+from lib.game import Game, ColumnFullException
 
 class GameTestCase(unittest.TestCase):
     def test_new_game(self):
@@ -14,6 +14,22 @@ class GameTestCase(unittest.TestCase):
         self.assertTrue(isinstance(g, Game))
         self.assertEquals(g._rows, 6)
         self.assertEquals(g._columns, 7)
+
+    def test_drop_piece_returns_final_position(self):
+        g = Game() # 6x7 by default
+        self.assertEquals(g._drop_piece('X', 0), (0,5))
+        self.assertEquals(g._drop_piece('X', 0), (0,4))
+        self.assertEquals(g._drop_piece('X', 0), (0,3))
+        self.assertEquals(g._drop_piece('O', 0), (0,2))
+        self.assertEquals(g._drop_piece('X', 0), (0,1))
+        self.assertEquals(g._drop_piece('X', 0), (0,0))
+
+    def test_drop_piece_on_full_column_raises(self):
+        g = Game(rows=2)
+        self.assertEquals(g._drop_piece('X', 0), (0,1))
+        self.assertEquals(g._drop_piece('X', 0), (0,0))
+        self.assertRaises(ColumnFullException, g._drop_piece, 'X', 0)
+
 
 if __name__ == "__main__":
     unittest.main()
